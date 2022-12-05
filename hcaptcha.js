4e05daa2581add2e06057f0713d2be4bb41fe8dd
Tom Nielsen <tanielsen@gmail.com>
2022-12-05 18:27:08 +0000

    hcaptcha

---
+++ hcaptcha.js
@@ -0,0 +1,113 @@
+const {
+  input,
+  div,
+  text,
+  script,
+  domReady,
+  textarea,
+  p,
+  style,
+  text_attr,
+  i,
+} = require("@saltcorn/markup/tags");
+const { verify } = require("hcaptcha");
+
+module.exports = {
+  name: "HCaptchaCertificate",
+  sql_name: "boolean",
+  attributes: [
+    {
+      name: "error_msg",
+      label: "Error message",
+      sublabel:
+        "This is shown to the user if the valued entered does not match.",
+      type: "String",
+    },
+    {
+      name: "sitekey",
+      label: "Sitekey",
+      sublabel: "HCaptcha site key",
+      type: "String",
+    },
+    {
+      name: "secret",
+      label: "Secret key",
+      sublabel: "HCaptcha account secret",
+      type: "String",
+    },
+  ],
+
+  validate:
+    ({ error_msg }) =>
+    (x) => {
+      if (!x) return { error: error_msg || "Incorrect captcha value" };
+      else return true;
+    },
+  read: (v) => {
+    return v;
+  },
+  postProcess: async (v, field) => {
+    if (typeof v === "boolean") return;
+    const secret = field.attributes.secret;
+    const token = v.success ? v.success : v;
+    const hres = await verify(secret, token);
+    if (hres.success) {
+      return { success: true };
+    } else {
+      return { error: field.attributes.error_msg || "verification failed" };
+    }
+  },
+
+  fieldviews: {
+    edit: {
+      isEdit: true,
+      run: (nm, v, attrs, cls, required, field) =>
+        input({
+          class: ["me-2 mt-1", cls],
+          "data-fieldname": field.name,
+          type: "checkbox",
+          onChange: attrs.onChange,
+          readonly: attrs.readonly,
+          name: nm,
+          id: `input${nm}`,
+          ...(v && { checked: true }),
+          ...(attrs.disabled && { onclick: "return false;" }),
+        }),
+    },
+    show: {
+      isEdit: false,
+      run: (v) =>
+        typeof v === "undefined" || v === null
+          ? ""
+          : v
+          ? i({
+              class: "fas fa-lg fa-check-circle text-success",
+            })
+          : i({
+              class: "fas fa-lg fa-times-circle text-danger",
+            }),
+    },
+    HCaptchaWidget: {
+      isEdit: true,
+      unsuitableAsAdminDefault: true,
+      readFromFormRecord: (rec, name) => {
+        if (rec["h-captcha-response"])
+          return { success: rec["h-captcha-response"] };
+        else return { error: "No hcaptcha response" };
+      },
+      run: (nm, v, attrs, cls, required, field) => {
+        return (
+          script({
+            src: "https://js.hcaptcha.com/1/api.js",
+            async: true,
+            defer: true,
+          }) +
+          div({
+            class: "h-captcha",
+            "data-sitekey": field.attributes.sitekey,
+          })
+        );
+      },
+    },
+  },
+};
--- index.js
+++ index.js
@@ -14,113 +14,119 @@ const { compareSync, hashSync } = require("bcryptjs");
 const svgCaptcha = require("svg-captcha");
 const { features } = require("@saltcorn/data/db/state");
 
+const types = [
+  {
+    name: "SvgCaptchaCertificate",
+    sql_name: "boolean",
+    readFromFormRecord: features?.async_validate
+      ? undefined
+      : (rec, name) => {
+          if (!rec[name]) return false;
+          if (!rec[name + "__hash"]) return false;
+          const cmp = compareSync(rec[name], rec[name + "__hash"]);
+          return cmp;
+        },
+    attributes: [
+      {
+        name: "error_msg",
+        label: "Error message",
+        sublabel:
+          "This is shown to the user if the valued entered does not match.",
+        type: "String",
+      },
+    ],
+    validate:
+      ({ error_msg }) =>
+      (x) => {
+        if (!x) return { error: error_msg || "Incorrect captcha value" };
+        else return true;
+      },
+    read: (v) => {
+      return v;
+    },
+    fieldviews: {
+      edit: {
+        isEdit: true,
+        run: (nm, v, attrs, cls, required, field) =>
+          input({
+            class: ["me-2 mt-1", cls],
+            "data-fieldname": field.name,
+            type: "checkbox",
+            onChange: attrs.onChange,
+            readonly: attrs.readonly,
+            name: nm,
+            id: `input${nm}`,
+            ...(v && { checked: true }),
+            ...(attrs.disabled && { onclick: "return false;" }),
+          }),
+      },
+      show: {
+        isEdit: false,
+        run: (v) =>
+          typeof v === "undefined" || v === null
+            ? ""
+            : v
+            ? i({
+                class: "fas fa-lg fa-check-circle text-success",
+              })
+            : i({
+                class: "fas fa-lg fa-times-circle text-danger",
+              }),
+      },
+      SvgCaptchaImage: {
+        isEdit: true,
+        unsuitableAsAdminDefault: true,
+        run: (nm) => {
+          var captcha = svgCaptcha.create();
+          return (
+            captcha.data +
+            input({
+              type: "hidden",
+              name: text_attr(nm) + "__hash",
+              id: `input${text_attr(nm)}__hash`,
+              value: hashSync(captcha.text, 10),
+            })
+          );
+        },
+      },
+      SvgCaptchaInput: {
+        isEdit: true,
+        unsuitableAsAdminDefault: true,
+        readFromFormRecord: (rec, name) => {
+          if (!rec[name]) return false;
+          if (!rec[name + "__hash"]) return false;
+          const cmp = compareSync(rec[name], rec[name + "__hash"]);
+          return cmp;
+        },
+        run: (nm, v, attrs, cls, required, field) => {
+          const errorFeedback =
+            typeof v === "string"
+              ? p(
+                  { class: "text-danger" },
+                  attrs?.error_msg || "Incorrect value"
+                )
+              : "";
+          return (
+            input({
+              type: "text",
+              class: "form-control",
+              name: text_attr(nm),
+              id: `input${text_attr(nm)}`,
+            }) + errorFeedback
+          );
+        },
+      },
+    },
+  },
+];
+
+if (features?.async_validate) {
+  types.push(require("./hcaptcha"));
+}
+
 module.exports = {
   sc_plugin_api_version: 1,
   plugin_name: "captcha",
   //headers,
-  types: [
-    {
-      name: "SvgCaptchaCertificate",
-      sql_name: "boolean",
-      readFromFormRecord: features?.async_validate
-        ? undefined
-        : (rec, name) => {
-            if (!rec[name]) return false;
-            if (!rec[name + "__hash"]) return false;
-            const cmp = compareSync(rec[name], rec[name + "__hash"]);
-            return cmp;
-          },
-      attributes: [
-        {
-          name: "error_msg",
-          label: "Error message",
-          sublabel:
-            "This is shown to the user if the valued entered does not match.",
-          type: "String",
-        },
-      ],
-      validate:
-        ({ error_msg }) =>
-        (x) => {
-          if (!x) return { error: error_msg || "Incorrect captcha value" };
-          else return true;
-        },
-      read: (v) => {
-        return v;
-      },
-      fieldviews: {
-        edit: {
-          isEdit: true,
-          run: (nm, v, attrs, cls, required, field) =>
-            input({
-              class: ["me-2 mt-1", cls],
-              "data-fieldname": field.name,
-              type: "checkbox",
-              onChange: attrs.onChange,
-              readonly: attrs.readonly,
-              name: nm,
-              id: `input${nm}`,
-              ...(v && { checked: true }),
-              ...(attrs.disabled && { onclick: "return false;" }),
-            }),
-        },
-        show: {
-          isEdit: false,
-          run: (v) =>
-            typeof v === "undefined" || v === null
-              ? ""
-              : v
-              ? i({
-                  class: "fas fa-lg fa-check-circle text-success",
-                })
-              : i({
-                  class: "fas fa-lg fa-times-circle text-danger",
-                }),
-        },
-        SvgCaptchaImage: {
-          isEdit: true,
-          unsuitableAsAdminDefault: true,
-          run: (nm) => {
-            var captcha = svgCaptcha.create();
-            return (
-              captcha.data +
-              input({
-                type: "hidden",
-                name: text_attr(nm) + "__hash",
-                id: `input${text_attr(nm)}__hash`,
-                value: hashSync(captcha.text, 10),
-              })
-            );
-          },
-        },
-        SvgCaptchaInput: {
-          isEdit: true,
-          unsuitableAsAdminDefault: true,
-          readFromFormRecord: (rec, name) => {
-            if (!rec[name]) return false;
-            if (!rec[name + "__hash"]) return false;
-            const cmp = compareSync(rec[name], rec[name + "__hash"]);
-            return cmp;
-          },
-          run: (nm, v, attrs, cls, required, field) => {
-            const errorFeedback =
-              typeof v === "string"
-                ? p(
-                    { class: "text-danger" },
-                    attrs?.error_msg || "Incorrect value"
-                  )
-                : "";
-            return (
-              input({
-                type: "text",
-                class: "form-control",
-                name: text_attr(nm),
-                id: `input${text_attr(nm)}`,
-              }) + errorFeedback
-            );
-          },
-        },
-      },
-    },
-  ],
+  types,
 };

c27465c4fb595bd5b4f390847cc24b2741134ba0
Tom Nielsen <tanielsen@gmail.com>
2022-12-05 16:47:58 +0000

    read on fv

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const {
   p,
   style,
   text_attr,
+  i,
 } = require("@saltcorn/markup/tags");
 const { compareSync, hashSync } = require("bcryptjs");
 const svgCaptcha = require("svg-captcha");
+const { features } = require("@saltcorn/data/db/state");
+
 module.exports = {
   sc_plugin_api_version: 1,
   plugin_name: "captcha",
@@ -19,12 +22,14 @@ module.exports = {
     {
       name: "SvgCaptchaCertificate",
       sql_name: "boolean",
-      readFromFormRecord: (rec, name) => {
-        if (!rec[name]) return false;
-        if (!rec[name + "__hash"]) return false;
-        const cmp = compareSync(rec[name], rec[name + "__hash"]);
-        return cmp;
-      },
+      readFromFormRecord: features?.async_validate
+        ? undefined
+        : (rec, name) => {
+            if (!rec[name]) return false;
+            if (!rec[name + "__hash"]) return false;
+            const cmp = compareSync(rec[name], rec[name + "__hash"]);
+            return cmp;
+          },
       attributes: [
         {
           name: "error_msg",
@@ -34,7 +39,6 @@ module.exports = {
           type: "String",
         },
       ],
-      skipAdminValidation: true,
       validate:
         ({ error_msg }) =>
         (x) => {
@@ -45,8 +49,37 @@ module.exports = {
         return v;
       },
       fieldviews: {
+        edit: {
+          isEdit: true,
+          run: (nm, v, attrs, cls, required, field) =>
+            input({
+              class: ["me-2 mt-1", cls],
+              "data-fieldname": field.name,
+              type: "checkbox",
+              onChange: attrs.onChange,
+              readonly: attrs.readonly,
+              name: nm,
+              id: `input${nm}`,
+              ...(v && { checked: true }),
+              ...(attrs.disabled && { onclick: "return false;" }),
+            }),
+        },
+        show: {
+          isEdit: false,
+          run: (v) =>
+            typeof v === "undefined" || v === null
+              ? ""
+              : v
+              ? i({
+                  class: "fas fa-lg fa-check-circle text-success",
+                })
+              : i({
+                  class: "fas fa-lg fa-times-circle text-danger",
+                }),
+        },
         SvgCaptchaImage: {
           isEdit: true,
+          unsuitableAsAdminDefault: true,
           run: (nm) => {
             var captcha = svgCaptcha.create();
             return (
@@ -62,6 +95,13 @@ module.exports = {
         },
         SvgCaptchaInput: {
           isEdit: true,
+          unsuitableAsAdminDefault: true,
+          readFromFormRecord: (rec, name) => {
+            if (!rec[name]) return false;
+            if (!rec[name + "__hash"]) return false;
+            const cmp = compareSync(rec[name], rec[name + "__hash"]);
+            return cmp;
+          },
           run: (nm, v, attrs, cls, required, field) => {
             const errorFeedback =
               typeof v === "string"
